cfc398c524fc0f798ea763f8fbc1c49a7184d090
IMS <jose8055@gmail.com>
2019-03-28 18:07:22 -0400

    Implemet UnitOfWork

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,8 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddAutoMapper();
             services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
